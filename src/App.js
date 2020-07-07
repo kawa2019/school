455ebcfa160710b66743637ff468d71ccbae02d4
kawa2019 <kamil.ku@vp.pl>
2020-07-07 14:52:03 +0200

    adding variables and fonts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import './scss/main.scss'
+import Home from "./components/Home"
 
-const App =()=> {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Home />
   );
 }
 
