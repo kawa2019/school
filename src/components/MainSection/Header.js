d54e1fbd684e58028d55a11d2cc8ffc2a6feec2b
kawa2019 <kamil.ku@vp.pl>
2020-07-13 15:26:18 +0200

    correct some bugs

--- src/components/MainSection/Header.js
+++ src/components/MainSection/Header.js
@@ -6,16 +6,13 @@ export default function Header() {
     const burgerMenu = useMediaQuery({
         query: '(max-width: 999px)'
     })
+    const arrayMenu = ["I stopnia", "II stopnia", "Podyplomowe", "Szkolenia", "Usługi"]
     return (
         <header>
             <img src={require("../../assets/image/UW-bialy.svg")} />
             {burgerMenu && <img onClick={() => setMenuSwitch(!menuSwitch)} width="50px" height="50px" src={require("../../assets/image/open-menu.svg")} />}
             <ul className={(menuSwitch && burgerMenu) ? "menu menuVisible" : "menu"}>
-                <li><a>I stopnia</a></li>
-                <li><a>II stopnia</a></li>
-                <li><a>Podyplomowe</a></li>
-                <li><a>Szkolenia</a></li>
-                <li><a>Usługi</a></li>
+                {arrayMenu.map(site => <li><a>{site}</a></li>)}
             </ul>
         </header>
     )
