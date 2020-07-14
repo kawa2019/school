74df426b2b72ece2acdaef2bd1d0062e78a57c17
kawa2019 <kamil.ku@vp.pl>
2020-07-14 09:51:14 +0200

    make closer sight to project, adding scroll menu

--- src/components/MainSection/Header.js
+++ src/components/MainSection/Header.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useMediaQuery } from 'react-responsive'
 
 export default function Header() {
     const [menuSwitch, setMenuSwitch] = useState("")
+    const [scroll, setScroll] = useState(false);
     const burgerMenu = useMediaQuery({
         query: '(max-width: 999px)'
     })
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        setScroll(window.scrollY > 80);
+      });
+    }, []);
+    const classNameMenu=()=>{
+        if (menuSwitch && burgerMenu){
+            return "menu menuVisible"
+        } else{
+            if (scroll){
+                return "menu fixedMenu" 
+            }
+            return "menu"
+        }
+    }
+    //position: fixed;
     const arrayMenu = ["I stopnia", "II stopnia", "Podyplomowe", "Szkolenia", "Usługi"]
     return (
         <header>
             <img src={require("../../assets/image/UW-bialy.svg")} />
             {burgerMenu && <img onClick={() => setMenuSwitch(!menuSwitch)} width="50px" height="50px" src={require("../../assets/image/open-menu.svg")} />}
-            <ul className={(menuSwitch && burgerMenu) ? "menu menuVisible" : "menu"}>
+            <ul className={classNameMenu()}>
                 {arrayMenu.map(site => <li><a>{site}</a></li>)}
             </ul>
         </header>
