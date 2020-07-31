9eb3bfbce976afe1df4641705dc2979ab1520dfe
kawa2019 <kamil.ku@vp.pl>
2020-07-31 11:17:06 +0200

    adding import font for optimalization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './assets/Muli/Muli-Bold.ttf'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
